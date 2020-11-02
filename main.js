da75ea52b9526a460b967368cdd922bc8f6352c7
Zjackeroo <jeffery_guo@hotmail.com>
2020-11-01 23:23:58 -0500

    .

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 import {AreaChart} from './AreaChart.js';
 import {StackedAreaChart} from './StackedAreaChart.js';
 
-var csvdata;
-var data = d3.csv('unemployment.csv', d3.autoType).then(data=>{
-    csvdata = data;
+d3.csv('unemployment.csv', d3.autoType).then(data=>{
+    
     console.log(data);
 
-    var columns = csvdata.columns.slice(1,-1);
+    var columns = data.columns.slice(1,-1);
 
-    csvdata.forEach(d=>{
+    data.forEach(d=>{
         let sum = 0; 
-        columns.forEach(col=>sum = sum + d[col]); 
+        columns.forEach(col => sum = sum + d[col]); 
         // console.log(sum); 
         d.total = sum;
     });
 
     var chart1 = AreaChart(".total_chart");
-    chart1.update(csvdata);
+    chart1.update(data);
     chart1.on("brushed", (range)=>{
         chart2.filterByDate(range); 
     });
 
     var chart2 = StackedAreaChart(".stacked_chart");
-    chart2.update(csvdata);
+    chart2.update(data);
 
     
 });
